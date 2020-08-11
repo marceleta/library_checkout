0b406eeab128d3c0e131c85dac3bf506970b08f3
Marcelo Paes Rocha <marcelo.rocha@nextsolucoes.net.br>
2020-08-11 17:17:58 -0300

    finalizado

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,16 @@
-{
-    'name': 'Library Book Borrowing',
-    'Description':'Members can borrow from the library',
-    'author':'Daniel Reis',
-    'depends': ['library_member','mail'],
-    'data':[
-        'security/ir.model.access.csv',
-        'views/library_menu.xml',
-        'views/checkout_view.xml',
-        'data/library_checkout_stage.xml'
-    ],
+{'name': 'Library Book Borrowing',
+ 'description': 'Members can borrow books from the library.',
+ 'author': 'Daniel Reis',
+ 'depends': [
+     'library_member',
+     'mail',
+ ],
+ 'data': [
+    'security/ir.model.access.csv',
+    'views/library_menu.xml',
+    'views/checkout_view.xml',
+    'views/checkout_kanban_view.xml',
+    'data/library_checkout_stage.xml',
+    'wizard/checkout_mass_message_wizard.xml',
+ ],
 }
